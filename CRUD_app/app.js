1c85f055b1066fb1bb27b86e27e28d5c63a2d6e6
fellowEngineer <bsuman1921@gmail.com>
2023-01-24 23:31:59 +0530

    CRUD Operation Project with Firebase, codebase setuped

---
+++ CRUD_app/app.js
@@ -0,0 +1,61 @@
+import { express } from "express";
+import {bodyParser} from "body-parser";
+
+
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
+
+
+app.get("/signin", (req, res) => {
+    res.render("signin");
+})
+app.post("/signin", (req, res) => {
+    res.render("signin");
+})
+
+
+
+app.get("/signup", (req, res) => {
+    res.render("signup");
+})
+app.post("/signup", (req, res) => {
+    res.render("signup");
+})
+
+
+
+app.get("/update", (req, res) => {
+    res.render("update");
+})
+app.post("/update", (req, res) => {
+    res.render("update");
+})
+
+
+
+app.get("/delete", (req, res) => {
+    res.render("delete");
+})
+app.post("/delete", (req, res) => {
+    res.render("delete");
+})
+
+
+
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log("server is running on PORT 4000.... \n");
+})
